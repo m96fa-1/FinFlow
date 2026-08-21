fd49d405506eaf314662f56732f9547838678b8f
m96fa-1 <mustafaalaliofficial@gmail.com>
2026-08-21 20:05:46 +0300

    Added categories route to index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import authRouter from './routes/auth'
+import categoriesRouter from './routes/categories'
 import transactionsRouter from './routes/transactions'
 import budgetsRouter from './routes/budgets'
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+app.use('/api/categories', categoriesRouter);
 app.use('/api/transactions', transactionsRouter);
 app.use('/api/budgets', budgetsRouter);
 
